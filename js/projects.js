7898e0ae833277d33184d905e46f6e5eff5ef0a6
Daan Di Scala <daandiscala@hotmail.com>
2026-06-23 01:06:05 +0200

    projects

---
+++ js/projects.js
@@ -0,0 +1,73 @@
+const projects = [
+  // === CURRENT PROJECTS ===
+  {
+    section: "current",
+    title: "EU-FarmBook",
+    image: "images/projects/farmbook.png",
+    goal: "An online platform to gather and share agriculture and forestry knowledge",
+     work: [ ["Tool", "the Farm Assistant chatbot"],
+        ["Research", `knowledge-driven conversational <a href=publications.html?filter=Recommender%20Systems target="_blank">recommender systems</a>`],
+        ["Conference", [["CLIN 2024", "posters.html"],["CLIN 2025", "posters.html"], ["USCKI 2026", "https://www.linkedin.com/posts/uscki-incognito_on-april-29-daan-di-scala-gave-a-caf%C3%A9-ki-ugcPost-7462460327822106624-38YO"], ["HHAI 2026", ""]]]
+    ],
+    video: "https://www.youtube-nocookie.com/embed/qO7ERjHYU9I?si=6NJnLtszeW2dR0Aw"
+  },
+
+  {
+    section: "current",
+    title: "TrustLLM",
+    image: "images/projects/trustllm2.png",
+    goal: "Democratising trustworthy and factual large language model technology for Europe",
+    work: [ 
+      ["Tool", "BijsluiterBot"], 
+      ["Research", "multi-step reasoning, structured knowledge extraction"],
+      ["Conference", [["ESWC 2024", "https://sites.google.com/view/genesy2024/"], ["NVFG 2026", "https://www.nvfg.nl/evenement/nvfg-regned-ai-in-de-farma/"]]]
+    ]
+  },
+
+  {
+    section: "current",
+    title: "LLMs4EU",
+    image: "images/projects/llms4eu.png",
+    goal: "Preserving Europe's linguistic diversity through accessible, multilingual AI models and tools",
+    work: [ ["Tool", "a knowledge-grounded advice tool to support AI evaluation"]]
+  },
+
+  // === PAST PROJECTS ===
+  {
+    section: "past",
+    title: "QuantiFarm",
+    image: "images/projects/quantifarm.png",
+    goal: "Assessing and recommending the impact of digital agriculture technologies for farmers",
+    work: [ 
+      ["Tool", "a knowledge-driven recommender tool"],
+      ["Conference", [["SEMANTiCS 2025", ""], ["ECIR 2026", ""]]]
+    ]
+  },
+
+  {
+    section: "past",
+    title: "CYCLOPES",
+    image: "images/projects/cyclopes.png",
+    goal: "Building an innovation-driven European law enforcement network to combat cybercrime",
+    work: [ ["Tool", "a QA tool for reliable, certainty-driven document analysis"]]
+  },
+
+  {
+    section: "past",
+    title: "Norm Engineering",
+    image: null,
+    goal: "Transforming complex legal rules into structured interpretations",
+    work: [ 
+      ["Research", "structured interpretations for legal question answering"],
+      ["Conference", [["CLIN 2025", "posters.html"]]]
+    ]
+  },
+
+  {
+    section: "past",
+    title: "DEFRAUDify",
+    image: null,
+    goal: "Developing a toolset to improve cybersecurity for businesses",
+    work: [ ["Tool", "an NLP tool for deduplicating documents via person disambiguation"]]
+  }
+];
