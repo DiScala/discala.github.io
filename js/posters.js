9aa907b6bb991190eda73aa140c1da668e938110
Daan Di Scala <daandiscala@hotmail.com>
2026-06-22 23:10:01 +0200

    update

---
+++ js/posters.js
@@ -0,0 +1,94 @@
+const posters = [
+
+    // ===== 2026 =====
+
+    {
+        year: 2026,
+        id: "hhai2026",
+        image: "images/posters/HHAI.png",
+        conference: "HHAI 2026",
+        authors: "<b>D.Di Scala</b>, M.De Boer",
+        takeaway:
+            "We introduce a method for evaluating trustworthiness of AI advice.",
+        paperText: "Corresponding Paper",
+        paperLink: "publications.html?id=Harvesting"
+    },
+
+
+    // ===== 2025 =====
+
+    {
+        year: 2025,
+        id: "farmbook2025",
+        image: "images/posters/FB poster.png",
+        conference: "CLIN 2025",
+        authors: "<b>D.Di Scala</b>, D.Vos, EU-FarmBook TNO Team",
+        takeaway:
+            "We study how knowledge-driven RAG can improve trustworthiness of agricultural AI advice. Part of the EU-FarmBook project.",
+        paperText: "Corresponding Paper (WIP)",
+        paperLink: null
+    },
+
+    {
+        year: 2025,
+        id: "normengineering2025",
+        image: "images/posters/NE Poster.png",
+        conference: "CLIN 2025",
+        authors: "<b>D.Di Scala</b>, R.Bakker, R.Van Drie, M.De Boer",
+        takeaway:
+            "We study how knowledge-driven RAG can improve trustworthiness of legal AI advice. Part of the Norm Engineering project.",
+        paperText: "Corresponding Paper (WIP)",
+        paperLink: null
+    },
+
+    {
+        year: 2025,
+        id: "vale2025",
+        image: "images/posters/VALE poster 3.png",
+        conference: "VALE@ECAI 2025",
+        authors: "<b>D.Di Scala</b>, M.De Boer, P.Yolum",
+        takeaway:
+            "We introduce an uncertainty-driven RAG (CERTA) and show how it answers more appropriately on QA tasks, answering 'I don't know' more often in the right moments.",
+        paperText: "Corresponding Paper",
+        paperLink: "publications.html?id=Towards%20Appropriate%20Trust"
+    },
+
+    {
+        year: 2025,
+        id: "trustai2025",
+        image: "images/posters/Poster ECAI.png",
+        conference: "TRUST-AI@ECAI 2025",
+        authors: "<b>D.Di Scala</b>, S.Lathouwers, M.Van Bekkum",
+        takeaway:
+            "We propose that the gap between functional and normative Trustworthy AI should be bridged with a descriptive framework.",
+        paperText: "Corresponding Paper",
+        paperLink: "publications.html?id=Bridging"
+    },
+
+
+    // ===== 2023 =====
+
+    {
+        year: 2023,
+        id: "aamas2023",
+        image: "images/posters/AAMAS Poster.png",
+        conference: "AAMAS 2023",
+        authors: "<b>D.Di Scala</b>, P.Yolum",
+        takeaway:
+            "We introduce PACCART, a framework for privacy protection through multi-agent argumentation.",
+        paperText: "Corresponding Paper",
+        paperLink: "publications.html?id=PACCART"
+    },
+        {
+        year: 2023,
+        id: "aamas2023",
+        image: "images/posters/AAMAS Poster.png",
+        conference: "AAMAS 2023",
+        authors: "<b>D.Di Scala</b>, P.Yolum",
+        takeaway:
+            "We introduce PACCART, a framework for privacy protection through multi-agent argumentation.",
+        paperText: "Corresponding Paper",
+        paperLink: "publications.html?id=PACCART"
+    }
+
+];
